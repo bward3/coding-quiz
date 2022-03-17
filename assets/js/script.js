b061a96dde1b1614e0209a385afe4050425d1ffa
Brian Ward <brianwardcs@gmail.com>
2022-03-16 22:08:53 -0600

    added game over functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 var quizContainer = document.getElementById("container");
 var timerEl = document.getElementById("timer");
 var questionNum = 0;
-var gameOver = false;
 var currentScore = 0;
 var secondsLeft = 20;
+var gameOn = false;
 
 var questions = [
     ["What is my favorite color?", 0],
@@ -26,9 +26,29 @@ function init() {
     beginEl.textContent = "Press button to start quiz.";
     var startBtn = document.createElement("button");
     startBtn.textContent = "Start";
-    startBtn.addEventListener("click", nextQuestion);
+    startBtn.addEventListener("click", startGame);
     quizContainer.append(beginEl);
     quizContainer.append(startBtn);
+}
+
+function startGame() {
+    timerEl.textContent = "Time: " + secondsLeft;
+    startTimer();
+    nextQuestion();
+    gameOn = true;
+}
+
+function startTimer() {
+
+    var timer = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = "Time: " + secondsLeft;
+
+        if (secondsLeft == 0 || gameOn === false) {
+            clearInterval(timer);
+            gameOver();
+        }
+    }, 1000);
 }
 
 function nextQuestion() {
@@ -41,28 +61,17 @@ function nextQuestion() {
     }
 }
 
-function gameOver() {
-    
-}
-
-function startTimer() {
-    
-    var timer = setInterval(function() {
-        timerEl.textContent = secondsLeft;
-        secondsLeft--;
-    });
-}
-
 function createQuestion(num) {
     //insert question to container div
     var questionText = questions[num][0];
     var correctAnswer = questions[num][1];
     var answerList = answers[num];
     var questionEl = document.createElement("h1");
+    var buttonList = document.createElement("ul");
+
     questionEl.textContent = questionText;
     quizContainer.append(questionEl);
 
-    var buttonList = document.createElement("ul");
     //generate answer buttons
     for (i = 0; i < answerList.length; i++) {
         var answerBtn = document.createElement("button");
@@ -84,14 +93,50 @@ function createQuestion(num) {
 function checkAnswer(event) {
     event.preventDefault();
     var answerEl = document.createElement("div");
+    answerEl.setAttribute("id", "correct-div");
     var btnClicked = event.target;
     var isCorrect = btnClicked.getAttribute("data-correct");
     if (isCorrect === "true") {
         currentScore += 5;
-        answerEl.textContent = "Correct!\aCurrent score: " + currentScore;
+        answerEl.innerHTML = "Correct!<br><br>Current score: " + currentScore;
     } else {
         answerEl.textContent = "Incorrect!";
     }
     nextQuestion();
     quizContainer.append(answerEl);
+}
+
+
+function gameOver() {
+    quizContainer.innerHTML = "";
+    timerEl.innerHTML = ""
+    gameOn = false;
+    enterScore();
+}
+
+function enterScore() {
+    quizContainer.classList.add("post-game")
+    var inputEl = document.createElement("input");
+    var submitBtn = document.createElement("input");
+    var promptDiv = document.createElement("div");
+    submitBtn.addEventListener("click", addScore);
+    inputEl.setAttribute("type", "text");
+    inputEl.setAttribute("id", "player-id");
+    submitBtn.setAttribute("type", "submit");
+    promptDiv.innerHTML = "<p>Type your initials and hit Enter</p>";
+    quizContainer.append(promptDiv);
+    quizContainer.append(inputEl);
+    quizContainer.append(submitBtn);
+}
+
+function addScore(event) {
+    event.preventDefault();
+    var inputEl = document.getElementById("player-id");
+    playerName = inputEl.value;
+    showScores(playerName);
+}
+
+function showScores(name) {
+    quizContainer.innerHTML = "";
+    console.log(name);
 }
