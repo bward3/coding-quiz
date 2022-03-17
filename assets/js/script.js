10dc819e50863511d2bc3c1c3e25b9b1fe66bec4
Brian Ward <brianwardcs@gmail.com>
2022-03-17 17:06:09 -0600

    finished quiz functionality. TODO add questions; bugfixes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,35 @@
 var quizContainer = document.getElementById("container");
 var timerEl = document.getElementById("timer");
-var questionNum = 0;
-var currentScore = 0;
-var secondsLeft = 20;
+var questionNum;
+var currentScore;
+var secondsLeft;
 var gameOn = false;
+const HISCORE_MAX = 10
+const SCORE_KEY = 'highScores';
 
 var questions = [
     ["What is my favorite color?", 0],
     ["What is the airspeed velocity of an unladen swallow?", 3],
     ["What is the capital of Turkey?", 0],
-    ["Where is the Taj Mahal located?", 0]
+    ["Where is the Taj Mahal located?", 0],
+
+    ["Inside which HTML element do we put the JavaScript?", 3],
+    ["What is the correct syntax for referring to an external script called \"xxx.js\"?", ]
+
+
+
 ]
 
 var answers = [
     ["Blue", "Green", "Yellow", "Orange"],
     ["25mph", "100m/s", "45mph", "Is it a European or African swallow?"],
     ["Istanbul", "Madrid", "Athens", "Constantinople"],
-    ["Agra", "Nagpur", "Kolkata", "Mumbai"]
+    ["Agra", "Nagpur", "Kolkata", "Mumbai"],
+
+    ["<scripting>", "<js>", "<code>", "<script>"],
+    ["<script name=\"xxx.js\">", "<script src=\"xxx.js\">", "<script href=\"xxx.js\">", "<script id=\"xxx.js\">"]
+
+    
 ]
 
 init();
@@ -27,15 +40,20 @@ function init() {
     var startBtn = document.createElement("button");
     startBtn.textContent = "Start";
     startBtn.addEventListener("click", startGame);
+    startBtn.classList.add("start-btn");
     quizContainer.append(beginEl);
     quizContainer.append(startBtn);
 }
 
 function startGame() {
+    gameOn = true;
+    questionNum = 0;
+    currentScore = 0;
+    secondsLeft = 60;
+    quizContainer.classList.remove("post-game");
     timerEl.textContent = "Time: " + secondsLeft;
     startTimer();
     nextQuestion();
-    gameOn = true;
 }
 
 function startTimer() {
@@ -76,8 +94,9 @@ function createQuestion(num) {
     for (i = 0; i < answerList.length; i++) {
         var answerBtn = document.createElement("button");
         var listItem = document.createElement("li");
-        answerBtn.textContent = (i + 1) + ". " + answers[num][i];
+        answerBtn.textContent = (i + 1) + ".  " + answers[num][i];
         answerBtn.setAttribute("id", "btn" + i);
+        answerBtn.style.paddingLeft = ".75rem"
         if (i === correctAnswer) {
             answerBtn.setAttribute("data-correct", "true");
         } else {
@@ -98,9 +117,9 @@ function checkAnswer(event) {
     var isCorrect = btnClicked.getAttribute("data-correct");
     if (isCorrect === "true") {
         currentScore += 5;
-        answerEl.innerHTML = "Correct!<br><br>Current score: " + currentScore;
+        answerEl.innerHTML = "<br>Correct!<br><br>Current score: " + currentScore;
     } else {
-        answerEl.textContent = "Incorrect!";
+        answerEl.innerHTML = "<br>Incorrect!<br><br>Current score: " + currentScore;
     }
     nextQuestion();
     quizContainer.append(answerEl);
@@ -111,32 +130,76 @@ function gameOver() {
     quizContainer.innerHTML = "";
     timerEl.innerHTML = ""
     gameOn = false;
+    questionNum=0;
     enterScore();
 }
 
 function enterScore() {
-    quizContainer.classList.add("post-game")
     var inputEl = document.createElement("input");
     var submitBtn = document.createElement("input");
     var promptDiv = document.createElement("div");
-    submitBtn.addEventListener("click", addScore);
+    submitBtn.addEventListener("click", submitEvent);
     inputEl.setAttribute("type", "text");
     inputEl.setAttribute("id", "player-id");
     submitBtn.setAttribute("type", "submit");
-    promptDiv.innerHTML = "<p>Type your initials and hit Enter</p>";
+    promptDiv.innerHTML = "<p>Type your initials to save your score</p>";
     quizContainer.append(promptDiv);
     quizContainer.append(inputEl);
     quizContainer.append(submitBtn);
 }
 
-function addScore(event) {
+function submitEvent(event) {
     event.preventDefault();
     var inputEl = document.getElementById("player-id");
     playerName = inputEl.value;
-    showScores(playerName);
+    checkHighScores(currentScore);
 }
 
-function showScores(name) {
+function checkHighScores(score) {
+    const HISCORE_STRING = localStorage.getItem(SCORE_KEY);
+    const SCORES = JSON.parse(HISCORE_STRING) ?? []; //set str to empty array if null
+    const LOWEST_SCORE = SCORES[HISCORE_MAX - 1]?.score ?? 0; //set lowest score to empty if 
+
+    if (score > LOWEST_SCORE) {
+        saveHighScore(score, playerName, SCORES);
+    } else {
+        drawScores();
+    }
+}
+
+function saveHighScore(score, name, scoreList) {
+    const newScore = { score, name };
+    scoreList.push(newScore);
+    scoreList.sort((a, b) => b.score - a.score);
+    scoreList.splice(HISCORE_MAX);
+    localStorage.setItem(SCORE_KEY, JSON.stringify(scoreList));
+    drawScores();
+}
+
+function drawScores() {
+    quizContainer.classList.add("post-game");
     quizContainer.innerHTML = "";
-    console.log(name);
+    var questionEl = document.createElement("h1");
+    var scoreList = document.createElement("ol");
+    questionEl.textContent = "HIGH SCORES";
+    const SCORES = JSON.parse(localStorage.getItem(SCORE_KEY)) ?? [];
+    console.log(SCORES);
+    scoreList.innerHTML = SCORES.map(
+        (pair) => `<li>${pair.score} - ${pair.name.toUpperCase()}</li>`
+    ).join('');
+    newGameBtn = document.createElement("button");
+    clearScoresBtn = document.createElement("button");
+    newGameBtn.textContent = "New Game";
+    clearScoresBtn.textContent = "Clear High Scores";
+    quizContainer.append(questionEl);
+    quizContainer.append(scoreList);
+    quizContainer.append(newGameBtn);
+    quizContainer.append(clearScoresBtn);
+    newGameBtn.addEventListener("click", startGame);
+    clearScoresBtn.addEventListener("click", clearScores);
+}
+
+function clearScores() {
+    localStorage.clear();
+    drawScores();
 }
